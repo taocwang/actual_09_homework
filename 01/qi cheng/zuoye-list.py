dc4158c2ce2b69d41435668b6e3b2c4d632d2f7f
qicheng5011 <qicheng5011@sina.com>
2016-04-18 18:41:27 +0800

    list列表查看两个最大数
    list列表查看两个最大数

---
+++ 01/qi cheng/zuoye-list.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+# Author : qicheng
+# Date : 2016/04/18 14:00
+#encoding:utf-8
+a = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45] 
+b = a[0] 
+c = a[0] 
+for i in a: 
+    if b < i: 
+       b = i 
+for i in a: 
+    if i != b and c < i: 
+       c = i 
+print b,c 

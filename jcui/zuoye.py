2efcef936dd1b966b0faf30822253f0fab70c1d4
VincentJcui <cui6522123@gmail.com>
2016-04-18 14:14:42 +0800

    更新测试脚本

--- jcui/zuoye.py
+++ jcui/zuoye.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+# by jcui
+# by 2016-04-18
 
 # sum = 0
 # num = 0
@@ -15,5 +17,5 @@
 #九九乘法表
 for x in range(1,10):
     print
-        for y in range(1,x+1):
-	        print  '%d x %d = %2d' % (x,y,x*y)  ,
+    for y in range(1,x+1):
+        print  '%d x %d = %2d' % (x,y,x*y)  ,

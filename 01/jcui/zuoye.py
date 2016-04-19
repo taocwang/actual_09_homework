fc41f857e8ae4452db5b74d6eeb9d62fd4ac8a0b
VincentJcui <cui6522123@gmail.com>
2016-04-19 22:32:11 +0800

    test1

--- 01/jcui/zuoye.py
+++ 01/jcui/zuoye.py
@@ -2,6 +2,7 @@
 # by jcui
 # by 2016-04-18
 
+#输入数字求平均数
 sum = 0
 num = 0
 n = 0
@@ -20,7 +21,7 @@ for x in range(1,10):
     for y in range(1,x+1):
         print  '%d x %d = %2d' % (x,y,x*y)  ,
 
-
+print "------------"
 #取列表最大的两个值
 list = [1,2,3,4,54,5,6,7,8,9,9,45]
 m = 0
@@ -31,3 +32,14 @@ for i in list:
     elif n < i <m:
         n = i
 print m,n
+
+#求10000块钱存多久可以翻倍
+
+sum = 10000
+n = 0
+while True:
+    sum = sum * (1+0.03)
+    if sum >= 20000:
+        break
+    n = n + 1
+print "%d年之后钱翻倍" % n

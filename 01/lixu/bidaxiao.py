cd356b191b7735834c71029e3f7f1fef360e3e93
lixu2016 <2994045080@qq.com>
2016-04-19 17:22:19 +0800

    first commit
    first commit

---
+++ 01/lixu/bidaxiao.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python2.7 python
+#Filename : bidaxiao.py
+#Author : LiXu
+#Date : 2016/04/18 
+
+nums = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45] 
+num = nums[0]
+num1 = nums[0]
+for i in nums:
+	if num < i :
+		num = i
+for j in nums:
+	if num != j  and num1 < j :
+                num1 = j
+print  num,num1

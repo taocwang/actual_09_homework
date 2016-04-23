17c511c41c6ff10cde5432e4b8aad251f9b776fe
VincentJcui <cui6522123@gmail.com>
2016-04-23 23:04:26 +0800

    test1111

--- 01/jcui/zuoye.py
+++ 01/jcui/zuoye.py
@@ -13,18 +13,20 @@ while num != '' :
         n = n + 1.0
     else:
         n = 1
+print "输入数字的个数及平均值------------------------------------------------"
 print sum / n
 
 
 #九九乘法表
+print "九九乘法表:------------------------------------------------"
 for x in range(1,10):
     print
     for y in range(1,x+1):
         print  '%d x %d = %2d' % (x,y,x*y)  ,
 
-print "------------"
+print "\n "
 #取列表最大的两个值
-list = [1,2,3,4,54,5,6,7,8,9,9,45,65535,558]
+list = [1,-2,-1,2,3,4,54,5,6,7,8,9,9,45,65535,558]
 m = 0
 n = 0
 for i in list:
@@ -32,7 +34,16 @@ for i in list:
         m = i
     elif n < i :
         n = i
+print "求最大的两个数:------------------------------------------------"
 print m,n
+#列表排序
+for i in range(0,len(list)):
+    for j in range(i+1,len(list)):
+        if list[i] > list[j]:
+            list[i],list[j] = list[j],list[i]
+print "列表排序:------------------------------------------------"
+print list
+
 
 #求10000块钱存多久可以翻倍
 
@@ -43,8 +54,18 @@ while True:
     if sum >= 20000:
         break
     n = n + 1
+print "存钱翻倍:------------------------------------------------"
 print "%d年之后钱翻倍" % n
 
+#求每个元素出现的次数
+nn = ['asd','afa','asd','arf','afas','asd','afa','arf']
+nm = {}
+for x in nn:
+    if x in nm:
+        nm[x] += 1
+    else:
+        nm[x] = 1
+print nm
 
 '''
 作业ok，在写代码时注意变量命名与python关键字和内置变量冲突，比如sum、list的使用，随课程介绍经验积累，加油

472d0b95685ee67c71dd20e7d605ae196799a94b
赵元海 <543551194@qq.com>
2016-04-24 18:18:40 +0800

    02

--- 01/zhaoyuanhai/a.py
+++ 01/zhaoyuanhai/a.py
@@ -4,7 +4,7 @@
 for i in range(1,10):
     for j in range(1,i+1):
 
-        print "%d * %d = %d" %(j, i, i * j),
+        print "%d * %d = %-2d" %(j, i, i * j),
     print ""
 
 
--- 01/zhaoyuanhai/numbers.py
+++ 01/zhaoyuanhai/numbers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #_*_coding:utf-8_*_
 
-li = [11, 999999, 280000, 9222, 1000000, 533433, 24444,4513456]
+li = [-5, -2, -9, -7, 10]
 
-num = [0, 0]
+num = [None, None]
 
 for i in li:
     if i > num[0]:

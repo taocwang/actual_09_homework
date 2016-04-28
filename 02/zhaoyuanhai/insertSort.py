50fce3915e3c26b476d390eeb8c8129ef2b70155
赵元海 <543551194@qq.com>
2016-04-28 17:12:26 +0800

    4

--- 02/zhaoyuanhai/insertSort.py
+++ 02/zhaoyuanhai/insertSort.py
@@ -15,6 +15,19 @@ print("result: " + str(l))
 n1 = int(raw_input("guess:"))
 L2 = [10,12,18,25,88,92,100,150]
 #L2 = [100, 88, 77, 55, 22, 11]
-      
+index1 = 0
+index2 = len(L2) - 1
+while (index1 <= index2):
+    mid = (index1 + index2) / 2
+    flag = L2[mid]
+    if n1 > flag:
+        index1 = mid + 1
+    elif n1 < flag:
+        index2 = mid -1
+    else:
+        print "number %s is found at index %s!!" %(n1,mid)
+        break
+else:
+    print "number %s is not fount!!!" % n1
     
 

8c2246a3e51626ff5e04ca31888b1d54f4e2660a
Administrator <543551194@qq.com>
2016-04-30 09:46:47 +0800

    5

--- 02/zhaoyuanhai/insertSort.py
+++ 02/zhaoyuanhai/insertSort.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #_*_coding:utf-8_*_
 
-#插入排序法
+#插入排序
 l = [4,1,9,13,34,26,10,7,4]
 for i in range(len(l)):
     for j in range(i+1,len(l)):
@@ -20,13 +20,13 @@ index2 = len(L2) - 1
 while (index1 <= index2):
     mid = (index1 + index2) / 2
     flag = L2[mid]
-    if n1 > flag:
-        index1 = mid + 1
+    if n1 == flag:
+        print "number %s is found at index %s!!" %(n1,mid)
+        break
     elif n1 < flag:
         index2 = mid -1
     else:
-        print "number %s is found at index %s!!" %(n1,mid)
-        break
+        index1 = mid + 1
 else:
     print "number %s is not fount!!!" % n1
     

6c3b16d41edd2172f5148c079f6b1f58974ba129
pk014 <zhaoyong014@163.com>
2016-05-07 10:27:35 +0800

    插入排序

--- 02/zhaoyong/insertSort.py
+++ 02/zhaoyong/insertSort.py
@@ -2,6 +2,16 @@
 
 list_num = [6,5,4,3,2,1]
 
+for i in range(0,len(list_num) -1):
+    for j in range(i,len(list_num)):
+        if list_num[i] > list_num[j]:
+            list_num[i],list_num[j] = list_num[j],list_num[i]
+            print list_num
+
+print '*' *100
+
+list_num = [6,5,4,3,2,1]
+
 for i in range(1,len(list_num)):
     for j in range(i,0,-1):
         if list_num[j-1] > list_num[j]:

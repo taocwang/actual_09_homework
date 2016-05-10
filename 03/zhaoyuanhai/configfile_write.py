538c3914ae4445f99481f256dbc46bc8ec14da4c
赵元海 <543551194@qq.com>
2016-05-10 15:51:26 +0800

    rev1

--- 03/zhaoyuanhai/configfile_write.py
+++ 03/zhaoyuanhai/configfile_write.py
@@ -14,7 +14,14 @@ server {{
         proxy_pass   http://127.0.0.1:{pro_port};
     }}
 }}
-'''
+'''.format(srv_port=9000, srv='server', srv_name='mysrv', srv_log='/var/log/nginx_access.log', srv_root='/var/www/html', pro_port=9999)
+fpath = '/Users/yhzhao/git/actual_09_homework/03/zhaoyuanhai/nginx.conf'
 
-print Nginx_Tem.format(srv_port=9000, srv='server', srv_name='mysrv', srv_log='/var/log/nginx_access.log', srv_root='/var/www/html', pro_port=9999)
+f = open(fpath, 'w')
+f.write(Nginx_Tem)
+f.close()
 
+f = open(fpath, 'r')
+text = f.read()
+f.close()
+print text

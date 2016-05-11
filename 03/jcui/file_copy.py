5f358e72e857dc5d63edd8b1ad4f7e43fb89ddf6
VincentJcui <cui6522123@gmail.com>
2016-05-11 15:17:44 +0800

    更新脚本

--- 03/jcui/file_copy.py
+++ 03/jcui/file_copy.py
@@ -1,1 +1,27 @@
 #encoding:utf-8
+
+import time
+
+
+def time_consuming():
+    def timess(func):
+        start = time.time()
+        func()
+        end = time.time()
+        print '用时:%.3fs' % (end-start)
+        return
+    return timess
+@time_consuming()
+def copy_file():
+    sptah='f:\\360yunpan.zip'
+    dpath='f:\\testcopy.zip'
+    sfile = open(sptah,'rb')
+    dfile = open(dpath,'wb')
+    while True:
+        data = sfile.read(4096)
+        if len(data) == 0:
+            sfile.close()
+            dfile.close()
+            break
+        else:
+            dfile.writelines(data)

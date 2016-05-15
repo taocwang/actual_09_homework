167604724396ed741cec2839048b4ff81726c4d8
arthaszyb <arthaszyb@users.noreply.github.com>
2016-05-15 11:16:49 +0800

    Update nginx_conf.py
    close加上()

--- 03/zhouyang/nginx_conf.py
+++ 03/zhouyang/nginx_conf.py
@@ -4,11 +4,11 @@
 file ='nginx.conf_default'
 handler=open(file,'r')
 content=handler.read()
-handler.close
+handler.close()
 
 content=content.format(server_port=8060,server_name='www.qqq.com',root_path='/usr/local/nginx/htdocs')
 
 new_file='nginx.conf'
 handle=open(new_file,'w')
 handle.write(content)
-handle.close
+handle.close()

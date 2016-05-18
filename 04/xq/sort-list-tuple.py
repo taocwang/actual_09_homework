fd056fb5813f072a386c56e35350fce03ec2701b
loveliuli <qunloveliliu@163.com>
2016-05-18 17:03:53 +0800

    home work the forth day

--- 04/xq/sort-list-tuple.py
+++ 04/xq/sort-list-tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-
-
 l=[(9,-2),(8,5),(3,4),(22,2),(0,8),(2,7),(0,1)]
 print "排序前：%s" %l
 
@@ -12,12 +10,12 @@ l=sorted(l,key=lambda x:max(x))
 print "方法一排序后:%s" %l
 
 #使用排序法
-x=0
-while x>(len(l)-1):
-    for i in l:
-        #if i[x][0] > i[x+1][0] or i[x][1] > i[x+1][1]:
-        if max(i)>max(i+1):
-            i[x],i[x+1] = i[x+1],i[x]
+x=len(l)
+while x>0:
+    for i in range(0,x-1):
+        if max(l[i])>max(l[i+1]):
+            l[i],l[i+1] = l[i+1],l[i]
+            #print l
     x-=1
 print "方法二排序后:%s" %l
 

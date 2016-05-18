b0b3c85048e1f99e9f511a2a03c47484359a5110
cui6522123@gmail.com <cui6522123@gmail.com>
2016-05-18 14:07:06 +0800

    第四课作业,update

--- 04/jcui/zuoye.py
+++ 04/jcui/zuoye.py
@@ -38,16 +38,78 @@ def list_sort(lists,reverse=False):
         for i in range(len(lists)-1):
             if max_num(lists[i],reverse=reverse) > max_num(lists[i+1],reverse=reverse):
                 lists[i], lists[i + 1] = lists[i + 1], lists[i]
-    print  lists
+    return  lists
 
 #随机生成列表
 def random_list(n):
     num_lists = []
     for i in range(n):
-        a = tuple(random.sample(range(0,100),2))
+        a = tuple(random.sample(range(0,100),random.randint(2,6)))
         num_lists.append(a)
     return num_lists
 
+
+def page_tmp(thead,tbody,reverse):
+    page_tmp = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>{title}</title>
+    </head>
+    <body>
+        <tr>
+            <h1>{title}</h1>
+        </tr>
+        <table border="1" align="left">
+            <thead>
+                <tr>
+                    {thead}
+                </tr>
+            </thead>
+            <tbody>
+                {tbody}
+            </tbody>
+            <tfoot>
+                <tr>
+
+                </tr>
+            </tfoot>
+        </table>
+
+    </body>
+    </html>
+    '''
+    if not reverse :
+        title = "按元祖内数字大小排序(按最大值排序)"
+    else:
+        title = "按元祖内数字大小排序(按最小值排序)"
+    return page_tmp.format(title = title,thead = thead,tbody = tbody)
+
+def create_page(x,reverse=False):
+    thead = '<th>名称</th>'
+    #原列表
+    tbody = '<th>原列表</th>'
+    for i in range(0,len(x)):
+        thead += '<th>%s</th>' % (i+1)
+        tbody += '<th>%s</th>' % str(x[i]).strip('()')
+
+    #原列表中每个元祖的最大值或最小值
+    tbody += '<tr><th>比较值</th>'
+    for i in range(0,len(x)):
+        tbody += '<th>%s</th>' % (max_num(x[i],reverse=reverse))
+    tbody += '</tr>'
+    #新列表
+    tbody += '<tr><th>新列表</th>'
+    new_list = list_sort(x,reverse=reverse)
+    for i in range(0,len(x)):
+        tbody += '<th>%s</th>' % str(new_list[i]).strip('()')
+    tbody += '</tr>'
+
+    #生成html
+    f = open('/home/op/test/index.html','w')
+    f.write(page_tmp(thead,tbody,reverse))
+    f.close()
 
 
 if __name__ == '__main__':
@@ -57,6 +119,8 @@ if __name__ == '__main__':
     list_sort(num_list2)
     x = random_list(10)
     #按元祖中的最大值进行排序
-    list_sort(x,reverse=False)
+    print list_sort(x)
     #按元祖中的最小值进行排序
-    list_sort(x, reverse=True)
+    print list_sort(x, reverse=True)
+    #生成页面
+    create_page(x,reverse=False)

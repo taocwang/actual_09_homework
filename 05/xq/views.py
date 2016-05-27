bff8e94d008da41ac7d6802c653e6116ac46f396
loveliuli <qunloveliliu@163.com>
2016-05-27 20:34:01 +0800

    home work for 5 days

--- 05/xq/views.py
+++ 05/xq/views.py
@@ -30,19 +30,16 @@ def login():
     params = request.args if request.method == 'GET' else request.form
     username =  params.get('username','')
     password =  params.get('password','')
-    print type('age')
     if user.validate_login(username,password):
         flash("%s,欢迎回来！" %username )
         return redirect('/show/')
     else:
         flash("用户名或密码错误，请重新输入！")
-        #return render_template('login.html',username=username,error='用户名或密码错误，请重新输入!')
         return render_template('login.html',username=username)
 
 @app.route('/show/')
 def show_user():
     user_list = user.get_user()
-    #print user_list
     return render_template("showuser.html",user_list=user_list)
 
 @app.route('/user/add/',methods=['POST','GET'])
@@ -54,11 +51,12 @@ def add_user():
         username =  params.get('username','')
         password =  params.get('password','')
         age =  params.get('age','')
+        #status,error = user.add_user(username,password,age)
         if user.add_user(username,password,age):
             flash("%s添加成功！" %username)
             return redirect('/show/')
         else:
-            return render_template("adduser.html",error="用户已存在或为空，请重新添加！")
+            return render_template("adduser.html",error="用户名已经存在或为空！")
 @app.route('/user/modify/',methods=['POST','GET'])
 def mod_user():
     params = request.args if request.method == 'GET' else request.form
@@ -79,24 +77,20 @@ def mod_user():
         age =  params.get('age','') 
         print "New:%s-%s-%s" %(username,password,age)
         print "Old:%s-%s-%s" %(username1,password1,age1)
-        if username != username1 and user.modify_user(username,password,age) != 'flag':
-            if user.del_user(username1,password1,age1):
-                flash("%s的信息已修改成功！" %username1)
-                if user.modify_user(username,password,age):
-                    return redirect('/show/')
-                else:
-                    return render_template("modifyuser.html",error="修改失败！")
+        #情况一：用户信息没变化
+        if user.modify_user(username,password,age) == 'same':
+             flash("%s信息没有变化" %username)
+             return redirect('/show/')
+        #情况二：修改后的用户名已经存在
         elif user.modify_user(username,password,age) == 'flag':
-            flash("修改失败，用户名已存在！")
-            return redirect("/show/")
-        else:
-            if user.modify_user(username,password,age) == 'same':
-                flash("%s信息没有变化！" %username)
-                return redirect('/show/')
-            elif user.modify_user(username,password,age):
-                flash("%s修改成功！" %username)
-                return redirect('/show/')
-            return render_template("modifyuser.html",error="修改失败！")
+            flash("用户名已存在，请重新修改！")
+            return redirect('/show/')
+        #情况三：其他情况，直接添加新记录，删掉老记录
+        user.del_user(username1,password1,age1)
+        if user.modify_user(username,password,age):
+            flash("%s修改成功！"%username)
+            return redirect('/show/')
+        return render_template("modifyuser.html",error="修改失败！")
 @app.route('/user/del/',methods=['POST','GET'])
 def del_user():
     username = request.args.get('username','')
@@ -111,7 +105,6 @@ def del_user():
 def finder_user():
     username = str(request.form.get('username',''))
     new_user = user.find_user(username)
-    #print new_user
     if user.find_user(username):
         return render_template('finduser.html',new_user = new_user)
     flash("Sorry，没有查到相关数据!")

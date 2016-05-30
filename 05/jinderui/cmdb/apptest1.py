6680c3fd828478bea8945e9012b2315f0cba092b
jinnery <deruijin@163.com>
2016-05-30 14:58:33 +0800

    update
    update

--- 05/jinderui/cmdb/apptest1.py
+++ 05/jinderui/cmdb/apptest1.py
@@ -18,7 +18,8 @@ def index():
 	# user_agent = request.headers.get('User-Agent')
 	return render_template('login.html')
 
-# 登陆成功后跳转到user页面。 登陆的时候进行了判断，通过外部函数user读取文件判断用户和密码是否正确。
+# 打开首页，跳转到登陆页面。成功后跳转到user页面。 
+#登陆的时候进行了判断，通过外部函数user读取文件判断用户和密码是否正确。(True就通过，False不通过)
 @app.route('/login/',methods=['POST','GET'])
 def login():
 	params = request.args if request.method == 'GET' else request.form
@@ -26,22 +27,21 @@ def login():
 	password = params.get('password','')
 
 	if user.validate_login(username=username,password=password): #判断用户名密码是否正确
-
 		return redirect('/users/')
 	else:
-		return render_template('login.html',error='用户名或密码错误')
+		return render_template('login.html',error='用户名或密码错误') #不正确提示信息通过渲染模板实现把error的值传到login.html里面
 
 #获取用户信息
 @app.route('/users/')
 def users():
-	user_list = user.get_user()
-	return render_template('users.html',user_list=user_list)
+	user_list = user.get_users()
+	return render_template('users.html',user_list=user_list) #渲染模板 通过for打印所有用户在users.html里面实现
 
 
-#通过users.html里面的添加按钮，跳转到 adduser页面进行添加用户信息
+#通过users.html里面的添加按钮，跳转到 adduser页面进行添加用户信息（form表单实现）
 @app.route('/adduser/',methods=['GET'])
 def adduser():
-	return render_template('adduser.html')  #adduser.html是个form表单。
+	return render_template('adduser.html') #跳转到adduser.html里面
 
 
 #通过adduser函数。添加完信息以后获取用户信息跳转到 用户users页面(存在的用户不让重复添加)
@@ -50,48 +50,39 @@ def commituser():
 	adduser = request.form.get('adduser')
 	addage = request.form.get('addage')
 	addpassword = request.form.get('password')
-	user_list = user.get_user()
 
-	for users in user_list:
-		if adduser in users['username']:
-			return render_template('adduser.html',user='用户名已存在')
-
-	if adduser and addage and addpassword:
-		user_list.append({'username':adduser,'age':addage,'password':addpassword})
-		user.add_user(json.dumps(user_list))
+	if user.validate_user(adduser,addage,addpassword):  #外部模块user.validate_user判断用户信息True执行
+		user.add_user(adduser,addage,addpassword)
 		return redirect('/users/')
-	else:
-		return render_template('adduser.html',info='信息不能为空')
+	else:												# 检查不通过 提示信息
+		return render_template('adduser.html',user='信息不正确')
 
-#删除用户通过get请求获取参数，进行判断。然后删除，并重新写入到文件。
+
+#删除用户通过get请求获取参数。然后把对应用户删掉
 @app.route('/deluser/')
 def deluser():
 	deluser = request.args.get('act')
-	user_list = user.get_user()
-
-	for users in user_list:
-		if users['username'] == deluser:
-			del user_list[user_list.index(users)]
-
-	user.add_user(json.dumps(user_list))
-
+	
+	user.del_user(deluser)       #通过user.del_user函数删掉得到的用户
 	return redirect('/users/')
 
 #更新用户先要把用户的信息带过去，这个地方用了get为了不让密码显示html里面写的是***
 @app.route('/updateuser/')
 def updateuser():
-	updateuser = request.args.get('actname')
-	updateage = request.args.get('actage')
-	updatepassword = request.args.get('actpassword')
+	username = request.args.get('actname')          #获取get actname的参数
+	_user = user.get_user(username)					#
+	if _user is None:
+		_error = '用户信息不存在'
+	else:
+		updateuser = _user.get('username')
+		updateage = _user.get('age')
+		updatepassword = _user.get('password')
 	return render_template('updateuser.html',updateuser=updateuser,updateage=updateage,updatepassword=updatepassword)
 
 
 '''
 通过updateuser函数获取原有的用户信息，用户名年龄。然后和要改的进行对比，发现age被修改了。
 重新赋值并写入（这个地方username是唯一的）所以说不能修改用户名
-此处没有更新密码，因为密码updateuser函数获取的密码都是***（用了get方式为了不让密码显示html里面写的是***）
-如果此处判断密码是否有修改就出问题了（因为我传的是***肯定和之前不一样了）。
-如果我不想修改密码呢，所以这个地方没有针对密码进行修改。正常应该写个专门修改密码的函数
 '''
 @app.route('/changeuser/',methods=['POST'])
 def changeuser():
@@ -99,25 +90,9 @@ def changeuser():
 	updateage = request.form.get('age')
 	updatepassword = request.form.get('password')
 
-	user_list=user.get_user()
-
-	for users in user_list:
-		if users['username'] == updateuser:
-			if users['age'] != updateage:
-				user_list[user_list.index(users)]['age'] = updateage
-				user.add_user(json.dumps(user_list))
-				return redirect('/users/')
-			else:
-				return redirect('/users/')
-
+	user.change_user(updateuser,updateage,updatepassword)
+	return redirect('/users/')
+	user_list=user.get_users()
+	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8888,debug=True)
-
-
-'''
-不错，自己代码里面多写注释
-改进点
-1. app中的代码逻辑越简单越好，只控制逻辑，不控制真实对数据的改写
-	将对用户的增删改查，验证等都写在user模块，由user模块提供统一的接口
-	如果后续要调整逻辑只需要调整user模块
-'''
--- 05/jinderui/cmdb/user.py
+++ 05/jinderui/cmdb/user.py
@@ -1,23 +1,75 @@
+#encoding: utf-8
 import json
 
-def get_user():
+#查询所有用户信息
+def get_users():
 	with open('userportal.txt','rb') as f:
 		cxt = f.read()
 		return json.loads(cxt)
 
-
+#检查 用户名密码是否正确。正确返回True 不正确返回False
 def validate_login(username,password):
-	users = get_user()
+	users = get_users()
 
 	for user in users:
-
 		if user.get('username') == username and user.get('password') == password:
 			return True
 
 	return False
 
+#检查用户信息，如果不满足就返回False。也就是检查不通过
+def validate_user(username,age,password):
+	if username.strip() == '':
+		return False, u'用户名不能为空'
 
-def add_user(user_list):
+	user_list = get_users()
+	for users in user_list:
+		if username in users['username']:
+			return False
+	
+	if len(password) < 6:
+		return False
+	if not str(age).isdigit() or int(age) <= 0 or int(age) > 100:
+		return False
+	return True
+
+#将信息保存到文件
+def save_user(user_list):
 	with open('userportal.txt','wb') as wf:
-		wf.write(user_list)
+		wf.write(json.dumps(user_list))
 
+#添加用户，调用save_uer并写入到文件
+def add_user(adduser,addage,addpassword):
+	user_list = get_users()
+	if adduser and addage and addpassword:
+		user_list.append({'username':adduser,'age':addage,'password':addpassword})
+
+	save_user(user_list)
+
+
+#删除用户，调用save_uer并重新写入到文件
+def del_user(deluser):
+	user_list = get_users()
+	for users in user_list:
+		if users['username'] == deluser:
+			del user_list[user_list.index(users)]
+	save_user(user_list)
+
+#更新用户，调用save_uer并重新写入到文件
+def change_user(updateuser,updateage,updatepassword):
+	user_list = get_users()
+	for users in user_list:
+		if users['username'] == updateuser:
+			if users['age'] != updateage:
+				user_list[user_list.index(users)]['age'] = updateage
+			if users['password'] != updatepassword:
+				user_list[user_list.index(users)]['password'] = updatepassword
+	save_user(user_list)
+
+#获取指定用户的信息
+def get_user(username):
+    _users = get_users()
+    for _user in _users:
+        if _user.get('username') == username:
+            return _user   #在for里面return的。所以每次返回的只是判断正确的用户信息（是一个字典）
+    return None

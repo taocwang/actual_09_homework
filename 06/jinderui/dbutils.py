0e5e49b62e0dfcd2c54c608031c9417990460000
jinnery <deruijin@163.com>
2016-06-02 01:38:27 +0800

    zuoye
    zuoye-cmdb

---
+++ 06/jinderui/dbutils.py
@@ -0,0 +1,39 @@
+import gconf
+import MySQLdb
+
+def execute_fetch_sql(sql,args=(),fetch=True):
+	return execute_sql(sql,args,fetch)
+
+def execute_commit_sql(sql,args=(),fetch=False):
+	return execute_sql(sql,args,fetch)
+
+def execute_del_sql(sql,args=(),fetch=False):
+	return execute_sql(sql,args,fetch)
+
+def execute_update_sql(sql,args=(),fetch=False):
+	return execute_sql(sql,args,fetch)
+
+def execute_sql(sql,args=(),fetch=True):  
+	_conn =None
+	_cur = None
+	_count = 0 
+	_rt_list =[]
+
+	try:
+		_conn = MySQLdb.connect(host=gconf.MYSQL_HOST,port=gconf.MYSQL_PORT, \
+				db=gconf.MYSQL_DB,user=gconf.MYSQL_USER, passwd=gconf.MYSQL_PASSWORD,charset=gconf.MYSQL_CHARSET)
+		_cur = _conn.cursor()
+		_count = _cur.execute(sql,args)
+		if fetch:
+			_rt_list = _cur.fetchall()
+		else:
+			_conn.commit()
+	except BaseException as e:
+		print e
+	finally:
+		if _cur:
+			_cur.close()
+		if _conn:
+			_conn.close()
+
+	return _count,_rt_list

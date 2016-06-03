593b6af72d9a0233f26245eb8f1d1c355a39f158
loveliuli <qunloveliliu@163.com>
2016-06-03 09:37:58 +0800

    home work for six days

---
+++ 06/xq/users-V3/dbutils.py
@@ -0,0 +1,33 @@
+#encoding: utf-8
+
+import MySQLdb
+
+import gconf
+
+
+def execute_sql(sql, args=(),fetch=True):
+    _conn = None
+    _cur = None
+    _count = 0
+    _rt_list = []
+    try:
+        #print sql
+        _conn = MySQLdb.connect(host=gconf.MYSQL_HOST, port=gconf.MYSQL_PORT, \
+                        user=gconf.MYSQL_USER, passwd=gconf.MYSQL_PASSWD, \
+                        db=gconf.MYSQL_DB, charset=gconf.MYSQL_CHARSET)
+
+        _cur = _conn.cursor()
+        _count = _cur.execute(sql, args)
+        if fetch is True:
+            _rt_list = _cur.fetchall()
+        else:
+            _conn.commit()
+    except BaseException as e:
+        print e
+    finally:
+        if _cur:
+            _cur.close()
+        if _conn:
+            _conn.close()
+
+    return _count, _rt_list

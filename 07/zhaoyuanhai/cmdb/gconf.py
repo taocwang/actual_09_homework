ef48536c20c55ef7723de6c54b892bb52723b3cf
ryan2333 <543551194@qq.com>
2016-06-09 15:50:35 +0800

    1

---
+++ 07/zhaoyuanhai/cmdb/gconf.py
@@ -0,0 +1,10 @@
+#encoding: utf-8
+
+USER_FILE = 'user.json'
+
+MYSQL_HOST = '180.153.191.128'
+MYSQL_PORT = 3306
+MYSQL_USER = 'reboot'
+MYSQL_PASSWD = 'reboot123'
+MYSQL_DB = 'kk2'
+MYSQL_CHARSET = 'utf8'

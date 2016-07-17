6ad9938a55d41a69cbea003dcc9fa557711a052b
Vincentjcui <cui6522123@gmail.com>
2016-07-17 19:30:54 +0800

    update

--- 11/jcui/12text.py
+++ 11/jcui/12text.py
@@ -11,7 +11,102 @@ _server_conn = smtplib.SMTP(server,port)
 _server_conn.ehlo()
 _server_conn.starttls()    启用ssl时需配置该选项
 _server_conn.sendmail('from','to','msg')
-_server_conn.close()
+_server_conn.quit()
 
+msg 依赖另外的包
+
+from email.mime.text import MIMEText
+
+
+
+psutil
+
+sudo pip install psutil
+
+获取服务器信息的第三方库
+
+爬虫:
+
+    requests
+        发起url请求,获取结果
+    pyquery
+        解析html
+    拼写url requests发起请求
+    PyQuery(text)
+    class 选择器
+    标签选择器
+    find
+
+    找到标签后干嘛?
+    1. attr attrib.get('attrname')
+    2. value .value
+    3. text .text
+
+
+'''
+
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+from pyquery import PyQuery as pyq
+import requests
+
+
+def sendmail(to_list,title,content):
+    smtp_host = 'smtp.tom.com'
+    smtp_port = 25
+    smtp_user = 'wolfccies@tom.com'
+    smtp_pass = 'wolfccies'
+
+    _server = smtplib.SMTP(smtp_host,smtp_port)
+    _server.set_debuglevel(True)
+    _server.ehlo()
+    _server.login(smtp_user,smtp_pass)
+    _msg = MIMEText(content,'html','utf-8')
+    _msg['Subject'] = title
+    _msg['To'] = ';'.join(to_list)
+    _msg['From'] = '51Reboot告警管理员<%s>' % smtp_user
+    _msg['Date'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    _server.sendmail(smtp_user,to_list,_msg.as_string())
+    _server.quit()
+
+
+def re_url(ip):
+    _response = requests.get('http://ip.chinaz.com/ip=%s' % ip)
+    _response.encoding = 'utf-8'
+    return _response
+
+def get_area(ip):
+    jq = pyq(re_url(ip).text)
+    rt = jq('.IcpMain02').find('.bor-b1s') .find('span')[-1].text
+    print rt
+
+def re_url2(ip):
+    _response = requests.get('http://ip138.com/ips138.asp?ip=%s' % ip)
+    _response.encoding = 'gbk'
+    return _response
+
+def get_area2(ip):
+    jq = pyq(re_url2(ip).text)
+    rt = jq('.ul1').find('li')[0].text
+    print rt
+    print type(rt)
+
+
+if __name__ == '__main__':
+    # sendmail(['cui6522123@163.com'],'告警邮件','cpu内存告警测试邮件')
+    ip = '221.179.216.54'
+    # get_area(ip)
+    get_area2(ip)
+
+
+
+'''
+作业:
+1.查看flask中获取参数值
+2.根据accesslog,统计code分布图
+3.画服务器cpu,内存使用率的仪表盘
+4.机房的增删改查
+5.爬虫抓取
 
 '''
---
+++ 11/jcui/agent.py
@@ -0,0 +1,119 @@
+#encoding:utf-8
+
+
+import logging
+import time
+import os
+import traceback
+
+import requests
+import json
+
+
+logger = logging.getLogger(__name__)
+App_key = 'c370af0f89fe66f06d1b70025d6a84c2'
+App_secret = 'a458f162b35f53343750e885ea9b6f73'
+server_url = 'http://localhost:9000/performs/'
+
+def execute_cmd(_cmd):
+    _fh = os.popen(_cmd)
+    _cxt = _fh.read()
+    _fh.close()
+    return _cxt.rstrip()
+
+def get_ip():
+    _cmd = "ifconfig ens33 | grep 'inet 地址' | awk '{print $2}' |awk -F':' '{print $2}'"
+    return execute_cmd(_cmd)
+
+def collect_cpu():
+    _cmd = "top -bn 1 |grep Cpu |awk '{print $8}'"
+    return 100 - float(execute_cmd(_cmd))
+
+def collect_ram():
+    f = open('/proc/meminfo','r')
+    _totol = float(f.readline().split()[1])
+    _free = float(f.readline().split()[1])
+    f.readline()
+    _buff = float(f.readline().split()[1])
+    f.close()
+    return 100*(_free+_buff)/_totol
+
+
+
+def collect():
+    rt = {}
+    rt['ip'] = get_ip()
+    rt['cpu'] = collect_cpu()
+    rt['ram'] = collect_ram()
+    rt['time'] = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    return rt
+
+def send(msg):
+    try:
+        _reponse = requests.post(server_url,data=json.dumps(msg),headers={"Content-Type":"application/json",'app_key':App_key,'app_secret':App_secret})
+        if not _reponse.ok:
+            logger.error('error send msg: %s',msg)
+        else:
+            _json = _reponse.json()
+            if _json.get('code') != 200:
+                logger.error('error send msg:%s ,result:%s',msg,_json)
+    except BaseException as e:
+        logger.error(traceback.format_exc())
+
+
+if __name__ == '__main__':
+    logging.basicConfig(Level=logging.DEBUG,
+                        format="%(asctime)s %(name)s %(pathname)s %(levelname)s %(message)s",
+                        filename='/home/jcui/temp/stdout.log')
+    while True:
+        try:
+            _msg = collect()
+            logger.debug(_msg)
+            send(_msg)
+            time.sleep(30)
+            logger.debug(time.strftime('%Y-%m-%d %H:%M:%S'))
+        except BaseException as e:
+            logger.error(traceback.format_exc())
+
+    '''
+    Content-Type application/json
+
+CREATE TABLE `performs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(128) DEFAULT NULL,
+  `cpu` float DEFAULT NULL,
+  `ram` float DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=218 DEFAULT CHARSET=utf8
+
+    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

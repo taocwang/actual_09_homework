b14af935600ef1293085e5a0a3e5a5fe2ef7b8f2
cui6522123@gmail.com <cui6522123@gmail.com>
2016-07-29 18:10:23 +0800

    更新模板

--- 12/jcui/cmdb/users/modules/access_log2db.py
+++ 12/jcui/cmdb/users/modules/access_log2db.py
@@ -3,8 +3,20 @@
 from dbutils import MySQLConnection as SQL
 import geoip2.database
 import time
+import urllib2
+import json
 
+def get_city(ip):
+    url = 'http://api.map.baidu.com/location/ip?ak=omi69HPHpl5luMtrjFzXn9df&ip=%s&coor=bd09ll' % ip
+    res = urllib2.Request(url)
+    html = json.loads(urllib2.urlopen(res).read())
+    if html['status'] == 0:
+        city = html.get('content').get('address_detail').get('city')
+        lat = html.get('content').get('point').get('y')
+        lng = html.get('content').get('point').get('x')
+        return [True,city,lat,lng]
 
+    return [False]
 
 if __name__ == '__main__':
     # logfile = '/home/jcui/files/www_access_20140823.log'
@@ -28,13 +40,33 @@ if __name__ == '__main__':
                 continue
             city = response.city.names.get('zh-CN','')
             if city == '':
-                print 'city is null:%s' % ip
-                continue
-            lat = response.location.latitude
-            lng = response.location.longitude
-            rt_list.append((logtime, ip, url, status, lat, lng, city))
+                _rt = get_city(ip)
+                if _rt[0]:
+                    city,lat,lng = _rt[1],_rt[2],_rt[3]
+                    if city == '':
+                        print 'city is null:%s' % ip
+                        continue
+                else:
+                    print 'city is null:%s' % ip
+                    continue
+            else:
+                lat = response.location.latitude
+                lng = response.location.longitude
+            #lng 经度
+            #lat 纬度
         except BaseException as e:
-            print 'geo ip not found ip:%s' % ip
+            print ip
+            _rt = get_city(ip)
+            if _rt[0]:
+                city, lat, lng = _rt[1], _rt[2], _rt[3]
+                if city == '':
+                    print 'city is null:%s' % ip
+                    continue
+            else:
+                print 'geo ip not found ip:%s' % ip
+
+        rt_list.append((logtime, ip, url, status, lat, lng, city))
+
     log_files.close()
     _sql = 'insert into access_logs2(logtime,ip,url,status,lat,lng,city) values(%s,%s,%s,%s,%s,%s,%s)'
     SQL.excute_log_sql(_sql,rt_list)

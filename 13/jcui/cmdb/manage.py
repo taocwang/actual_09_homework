e5f5352785b64e4f440681f190ed731a973423f9
cui6522123@gmail.com <cui6522123@gmail.com>
2016-08-01 15:58:00 +0800

    更新地图js

--- 13/jcui/cmdb/manage.py
+++ 13/jcui/cmdb/manage.py
@@ -6,3 +6,4 @@ from users import app
 if __name__ == '__main__':
     # print app.url_map
     app.run(host='0.0.0.0', port=9000, debug=True)
+    #uwsgi --http 0.0.0.0:9000 --wsgi-file Manager.py --callable app --processes 4 --threads 4
